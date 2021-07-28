2d7ac1ef0ae6f6d1d8cd9512b82cdde46e0e139b
Matteo Gramegnatota <m.gramegnatota@77agency.com>
2021-07-29 01:45:58 +0200

    Edited bash file

---
+++ Basic Gulp Theme/footer.php
@@ -0,0 +1,6 @@
+<?php
+	include_once(locate_template( 'template_parts/footer-template.php' ));
+	wp_footer();
+?>
+</body>
+</html>
---
+++ Basic Gulp Theme/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php wp_title('|', true, 'right'); ?></title>
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php include( locate_template('template_parts/main-menu.php') );
