634f9d7d5d7497dd7443901c9b086a1d9b8a6851
Matteo Gramegnatota <m.gramegnatota@77agency.com>
2022-05-03 11:22:06 +0200

    Modified wp-setup.sh

--- Base Gulp Theme/homepage.php
+++ Base Gulp Theme/homepage.php
@@ -6,24 +6,16 @@
 get_header();
 ?>
 
-<h1>Homepage</h1>
+
+<main class="main">
+
+	<?php include(locate_template('template_parts/breadcrumb.php')); ?>
+
+</main>
 
 
 <?php get_footer();
 
-/* TASK: Creating a server with Browser-sync & Enabling Auto-reloading */
-const server = browserSync.create();
-export const serve = done => {
-    server.init({
-        proxy: `https://localhost/${info.name}`
-    });
-    done();
-};
-export const reload = done => {
-    server.reload();
-    done();
-};
-// ------------------------------------------------
 
 
 
